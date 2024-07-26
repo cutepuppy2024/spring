f150959ca0d28881add05a822680a2871978922f
cutepuppy2024 <162073648+cutepuppy2024@users.noreply.github.com>
2024-07-26 13:28:21 +0900

    Add files via upload

---
+++ p0722_02_gradle/src/main/java/com/java/P072202GradleApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P072202GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P072202GradleApplication.class, args);
+	}
+
+}
