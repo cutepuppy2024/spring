08ec2ba6c313dda3aee6f9899a1b34c9ee8fa405
cutepuppy2024 <162073648+cutepuppy2024@users.noreply.github.com>
2024-07-26 13:27:53 +0900

    Add files via upload

---
+++ p0725_01_aop/src/main/java/com/java/P072501Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P072501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P072501Application.class, args);
+	}
+
+}
